ef1d19d5294e897c1fd51165b8a93d65545593f2
izabella <bellaiza631@gmail.com>
2018-11-16 14:54:38 -0500

    first commit

---
+++ js/design.js
@@ -0,0 +1,14 @@
+$('.menu-toggle').click(function() {
+
+  $('.site-nav').toggleClass('site-nav--open', 500);
+  $(this).toggleClass('open');
+
+})
+
+$("#button-cs").on('click', function(){
+     window.location = "project.html";
+});
+
+$("#button-prototype").on('click', function(){
+     window.location = "https://invis.io/HQJ5R2PZPDU";
+});
